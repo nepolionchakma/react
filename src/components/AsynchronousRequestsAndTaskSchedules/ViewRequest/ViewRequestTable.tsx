d4a81f46134a245154c3ddf910f1c5a610dc901e
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-05-30 13:58:24 +0600

    added previous time out value

--- src/components/AsynchronousRequestsAndTaskSchedules/ViewRequest/ViewRequestTable.tsx
+++ src/components/AsynchronousRequestsAndTaskSchedules/ViewRequest/ViewRequestTable.tsx
@@ -89,7 +89,7 @@ export function ViewRequestTable() {
       fetchData();
       table.toggleAllPageRowsSelected(false);
       setRowSelection({});
-    });
+    }, 1000);
 
     return () => clearTimeout(delayDebounce); // Cleanup timeout
   }, [query, page]); // Run on query and page change
