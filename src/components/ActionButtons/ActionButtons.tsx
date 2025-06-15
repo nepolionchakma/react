cac46670539f14444679e2ebaf543d4df190570c
Jyanial Dewan <djyanial@gmail.com>
2025-06-15 12:55:34 +0600

    new components

---
+++ src/components/ActionButtons/ActionButtons.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ActionButtonsProps {
+  children: React.ReactNode;
+}
+
+function ActionButtons({ children }: ActionButtonsProps) {
+  return (
+    <div className="flex gap-3 items-center px-4 py-2 border rounded">
+      {children}
+    </div>
+  );
+}
+
+export default ActionButtons;
