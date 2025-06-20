f3a7ac1666aa4dc43bb8f377a24912fe8659922f
kyaching <kyochingmarma@gmail.com>
2025-06-20 13:28:43 +0600

    fixed the design

--- src/components/CustomDropDown/CustomDropDown.tsx
+++ src/components/CustomDropDown/CustomDropDown.tsx
@@ -2,17 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import { Check, ChevronDown } from "lucide-react";
 
 interface IDropDownProps {
-  profileType: string;
-  setProfileId: React.Dispatch<React.SetStateAction<string>>;
-  setProfileType: React.Dispatch<React.SetStateAction<string>>;
+  data: string[];
+  option: string;
+  setOption: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const profileTypes = ["Email", "Mobile Number", "GUID"];
-
 export default function CustomDropDown({
-  profileType,
-  setProfileType,
-  setProfileId,
+  data,
+  option,
+  setOption,
 }: IDropDownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [hovered, setHovered] = useState(false);
@@ -35,8 +33,8 @@ export default function CustomDropDown({
   };
 
   const handleChange = (item: string) => {
-    setProfileType(item);
-    setProfileId("");
+    setOption(item);
+    // setProfileId("");
     setIsOpen(false);
   };
   return (
@@ -47,7 +45,7 @@ export default function CustomDropDown({
         className="flex justify-between items-center h-10 py-2 px-3 border rounded-md w-full text-left"
       >
         <span className="text-sm">
-          {profileType ? profileType : "Select Profile Type"}
+          {option ? option : "Select Profile Type"}
         </span>
         <ChevronDown className="h-4 w-4 opacity-50" />
       </button>
@@ -57,17 +55,15 @@ export default function CustomDropDown({
           ref={ref}
           className="flex flex-col items-start border rounded-md shadow-md p-1"
         >
-          {profileTypes.map((name, i) => {
+          {data.map((name, i) => {
             return (
               <button
                 type="button"
                 className={`flex items-center  py-2 px-2 w-full text-left text-sm rounded-md ${
-                  name === profileType && !hovered
+                  name === option && !hovered
                     ? "bg-slate-100"
                     : "hover:bg-slate-100"
-                } ${
-                  name !== profileType && hovered ? "hover:bg-slate-100" : ""
-                }`}
+                } ${name !== option && hovered ? "hover:bg-slate-100" : ""}`}
                 key={i}
                 onClick={() => handleChange(name)}
                 onMouseEnter={() => {
@@ -78,7 +74,7 @@ export default function CustomDropDown({
                 }}
               >
                 <span className="w-4">
-                  {name == profileType && <Check size={16} />}
+                  {name == option && <Check size={16} />}
                 </span>
                 <span className="ml-2">{name}</span>
               </button>
