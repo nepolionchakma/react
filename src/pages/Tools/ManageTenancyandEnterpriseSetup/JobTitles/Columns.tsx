d35bdfe2ec72aa9e44045ed45d6abf238c2ce19d
Jyanial Dewan <djyanial@gmail.com>
2026-03-26 09:16:51 +0600

    changed

--- src/pages/Tools/ManageTenancyandEnterpriseSetup/JobTitles/Columns.tsx
+++ src/pages/Tools/ManageTenancyandEnterpriseSetup/JobTitles/Columns.tsx
@@ -1,9 +1,10 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { IJobTitle, ITenantsTypes } from "@/types/interfaces/users.interface";
+import { IJobTitle, Users } from "@/types/interfaces/users.interface";
 import { ArrowUpDown } from "lucide-react";
-import { renderTenantsNames } from "@/Utility/general";
+import { convertDate } from "@/Utility/DateConverter";
+import { renderUserName } from "@/Utility/NotificationUtils";
 
-export const columns = (tenants: ITenantsTypes[]): ColumnDef<IJobTitle>[] => [
+export const columns = (users: Users[]): ColumnDef<IJobTitle>[] => [
   {
     id: "select",
     size: 24,
@@ -14,7 +15,7 @@ export const columns = (tenants: ITenantsTypes[]): ColumnDef<IJobTitle>[] => [
     enableResizing: false,
   },
   {
-    accessorKey: "tenant_id",
+    accessorKey: "job_title_id",
     enableResizing: true,
     sortingFn: (rowA, rowB, columnId) => {
       const a = rowA.getValue(columnId) as number;
@@ -25,19 +26,17 @@ export const columns = (tenants: ITenantsTypes[]): ColumnDef<IJobTitle>[] => [
     header: ({ column }) => {
       return (
         <div
+          className="min-w-max"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          className="min-w-max cursor-pointer"
         >
-          Tenant Name
-          <ArrowUpDown className="ml-2 h-4 w-4 inline-block" />
+          Job Title Id
+          <ArrowUpDown className="ml-2 h-4 w-4 cursor-pointer inline-block" />
         </div>
       );
     },
+
     cell: ({ row }) => (
-      <div className="capitalize min-w-max">
-        {renderTenantsNames(row.original.tenant_id, tenants)}
-        {/* <span>Id {row.getValue("tenant_id")}</span> */}
-      </div>
+      <div className="capitalize min-w-max">{row.getValue("job_title_id")}</div>
     ),
   },
   {
@@ -61,8 +60,118 @@ export const columns = (tenants: ITenantsTypes[]): ColumnDef<IJobTitle>[] => [
       );
     },
     cell: ({ row }) => (
-      <div className="capitalize min-w-[60rem]">
+      <div className="capitalize min-w-[20rem]">
         {row.getValue("job_title_name")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "created_by",
+    enableResizing: true,
+    sortingFn: (rowA, rowB, columnId) => {
+      const a = renderUserName(rowA.getValue(columnId), users) as string;
+      const b = renderUserName(rowB.getValue(columnId), users) as string;
+
+      return a.localeCompare(b, undefined, { sensitivity: "base" });
+    },
+    header: ({ column }) => {
+      return (
+        <div
+          className="min-w-max"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Created By
+          <ArrowUpDown className="ml-2 h-4 w-4 cursor-pointer inline-block" />
+        </div>
+      );
+    },
+
+    cell: ({ row }) => (
+      <div className="capitalize min-w-max">
+        {renderUserName(row.getValue("created_by"), users)}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "creation_date",
+    enableResizing: true,
+    sortingFn: (rowA, rowB, columnId) => {
+      const a = rowA.getValue(columnId) as Date;
+      const b = rowB.getValue(columnId) as Date;
+
+      return a.getTime() - b.getTime();
+    },
+    // header: "Username",
+    header: ({ column }) => {
+      return (
+        <div
+          className="min-w-max"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Creation Date
+          <ArrowUpDown className="ml-2 h-4 w-4 cursor-pointer inline-block" />
+        </div>
+      );
+    },
+
+    cell: ({ row }) => (
+      <div className="capitalize min-w-max">
+        {convertDate(row.getValue("creation_date"))}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "last_updated_by",
+    enableResizing: true,
+    sortingFn: (rowA, rowB, columnId) => {
+      const a = renderUserName(rowA.getValue(columnId), users) as string;
+      const b = renderUserName(rowB.getValue(columnId), users) as string;
+
+      return a.localeCompare(b, undefined, { sensitivity: "base" });
+    },
+    header: ({ column }) => {
+      return (
+        <div
+          className="min-w-max"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Updated By
+          <ArrowUpDown className="ml-2 h-4 w-4 cursor-pointer inline-block" />
+        </div>
+      );
+    },
+
+    cell: ({ row }) => (
+      <div className="capitalize min-w-max">
+        {renderUserName(row.getValue("last_updated_by"), users)}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "last_update_date",
+    enableResizing: true,
+    sortingFn: (rowA, rowB, columnId) => {
+      const a = rowA.getValue(columnId) as Date;
+      const b = rowB.getValue(columnId) as Date;
+
+      return a.getTime() - b.getTime();
+    },
+    // header: "Username",
+    header: ({ column }) => {
+      return (
+        <div
+          className="min-w-max"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Last Update Date
+          <ArrowUpDown className="ml-2 h-4 w-4 cursor-pointer inline-block" />
+        </div>
+      );
+    },
+
+    cell: ({ row }) => (
+      <div className="capitalize min-w-max">
+        {convertDate(row.getValue("last_update_date"))}
       </div>
     ),
   },
